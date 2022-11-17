3505417727cd3adf62780b4336957322e9c2c95f
Joe <28671159+JoeYe-233@users.noreply.github.com>
2022-11-17 20:56:52 +0800

    1

--- debug.py
+++ debug.py
@@ -9,7 +9,7 @@ args = parser.parse_args()
 
 print("参数信息：", args)
 
-gpg=gnupg.GPG(gnupghome='$HOME/.gpg/keys')
+gpg=gnupg.GPG(gnupghome='/home/runner/.gpg/keys')
 data="Hello world"
 import_result = gpg.import_keys(args.publicKey)
 fingerprints=import_result.fingerprints

9d333a66db80854d23a8fc6e01c84f870c33c991
Joe <28671159+JoeYe-233@users.noreply.github.com>
2022-11-17 18:46:18 +0800

    1

---
+++ debug.py
@@ -0,0 +1,18 @@
+import argparse
+import gnupg
+
+
+
+parser = argparse.ArgumentParser(description='manual to this script')
+parser.add_argument('--publicKey', type=str, default=None)
+args = parser.parse_args()
+
+print("参数信息：", args)
+
+gpg=gnupg.GPG()
+data="Hello world"
+import_result = gpg.import_keys_file("public.asc")
+fingerprints=import_result.fingerprints
+encrypted_data = gpg.encrypt(data,fingerprints)
+print(encrypted_data)
+

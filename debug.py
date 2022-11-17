d9862e6fea2dc2a43f71eca2bff213a48833d2a6
Joe <28671159+JoeYe-233@users.noreply.github.com>
2022-11-17 19:54:36 +0800

    done

--- debug.py
+++ debug.py
@@ -15,6 +15,6 @@ import_result = gpg.import_keys(args.publicKey)
 fingerprints=import_result.fingerprints
 print("添加了",import_result.count,"个密钥对")
 print(fingerprints)
-# encrypted_data = gpg.encrypt(data,fingerprints)
-# print(encrypted_data)
+encrypted_data = gpg.encrypt(data,fingerprints)
+print(encrypted_data)
 

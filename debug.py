42ee502dcf0af1ca855ca83414601abe15b2eabf
Joe <28671159+JoeYe-233@users.noreply.github.com>
2022-11-17 20:20:23 +0800

    1

--- debug.py
+++ debug.py
@@ -13,6 +13,7 @@ gpg=gnupg.GPG()
 data="Hello world"
 import_result = gpg.import_keys(args.publicKey)
 fingerprints=import_result.fingerprints
+gpg.trust_keys(fingerprints, 'TRUST_ULTIMATE')
 print("添加了",import_result.count,"个密钥对")
 print(fingerprints)
 encrypted_data = gpg.encrypt(data,fingerprints,sign=True)

85cfbd0f16415d397fb51afc6b3fbc8956d01840
Joe <28671159+JoeYe-233@users.noreply.github.com>
2022-11-18 11:08:53 +0800

    1

--- debug.py
+++ debug.py
@@ -1,6 +1,6 @@
 import argparse
 import gnupg
-
+import os
 
 
 parser = argparse.ArgumentParser(description='manual to this script')
@@ -13,7 +13,8 @@ gpg=gnupg.GPG(gnupghome='/home/runner/.gpg/keys')
 data="Hello world"
 import_result = gpg.import_keys(args.publicKey)
 fingerprints=import_result.fingerprints
-gpg.trust_keys(fingerprints, 'TRUST_ULTIMATE')
+os.system("gpg --edit-key \'"+fingerprints+"\'")
+# gpg.trust_keys(fingerprints, 'TRUST_ULTIMATE')
 print("添加了",import_result.count,"个密钥对")
 print(fingerprints)
 encrypted_data = gpg.encrypt(data,fingerprints,sign=True)

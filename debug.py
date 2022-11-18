073cd4c6d01d9c265d088b9dd707cb1832314b27
Joe <28671159+JoeYe-233@users.noreply.github.com>
2022-11-18 13:52:43 +0800

    Code Cleanup

--- debug.py
+++ debug.py
@@ -7,7 +7,7 @@ parser = argparse.ArgumentParser(description='manual to this script')
 parser.add_argument('--publicKey', type=str, default=None)
 args = parser.parse_args()
 
-print("参数信息：", args)
+# print("参数信息：", args)
 
 gpg=gnupg.GPG()
 data="Hello world"
@@ -16,17 +16,13 @@ fingerprints=import_result.fingerprints
 os.system("gpg --edit-key \'"+fingerprints[0]+"\'")
 #os.system("gpg --import-ownertrust "+fingerprints[0]+":6:")
 os.system("echo \""+fingerprints[0]+":6:\" | gpg --import-ownertrust")
-os.system("gpg --list-keys")
+# os.system("gpg --list-keys")
 # gpg.trust_keys(fingerprints, 'TRUST_ULTIMATE')
 print("添加了",import_result.count,"个密钥对")
-
-
-print(fingerprints[0])
-
-
+print("密钥指纹：",fingerprints[0])
 
 encrypted_data = gpg.encrypt(data,fingerprints)
 # print(encrypted_data.status_detail)available after python-gnupg 0.5.1
 print(encrypted_data.status)
-print(encrypted_data.ok)
+# print(encrypted_data.ok)
 print(str(encrypted_data))
